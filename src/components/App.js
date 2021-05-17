b609bc04bd5f6298531268963c904f677c1a456f
Paweł Jankowski <paweljankowski92@gmail.com>
2021-05-17 13:59:28 +0200

    authoryzation

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,15 @@
 import React, { Component } from 'react';
-import Textarea from './Textarea';
 import Password from './Password';
 import './App.css';
 
 class App extends Component {
-  state = {
-    
-  }
 
   render() {
+
     return (
       <div className="App">
         <h1>Cypher code</h1>
         <Password />
-        <Textarea />
       </div>
     );
   }
--- src/components/Password.js
+++ src/components/Password.js
@@ -1,17 +1,51 @@
 import React, { Component } from 'react';
+import Textarea from './Textarea';
+
 
 class Password extends Component {
 
-  access = () => {
-    console.log('działa access')
+  state = {
+    password: false,
+    access: false,
   }
+
+
+
+  access = () => {
+    const input = document.getElementById('pass');
+    const password = this.state.password;
+    
+    if(password === true){
+      this.setState({
+        access: true,
+      })
+    } else {
+      alert("Nieprawidłowe hasło. Podpowiedź: Producent oprogramowania w Poznaniu")
+    }
+    input.value = '';
+
+  }
+
+  handleText = (e) => {
+    const value = e.target.value;
+    console.log(value)
+    if(value === '!syn@psi.xyz'){
+      this.setState({
+        password: true,
+      })
+  }
+}
 
 render() {
   return (
     <div>
       <h1>Autorization</h1>
-      <input type="text" placeholder="wpisz hasło..." />
+      <input id="pass" type="password" placeholder="wpisz hasło..." onChange={this.handleText}/>
       <button onClick={this.access}>Uzyskaj dostęp</button>
+      {!this.state.access ? null :
+        <div><p className='correct'>Hasło prawidłowe, zapraszam do zapoznania się z generatorem szyfru :)</p><Textarea />
+        </div>
+      }
     </div>
   )
 }
